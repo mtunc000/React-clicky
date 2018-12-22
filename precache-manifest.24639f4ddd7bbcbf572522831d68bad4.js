e2c0508e71c25717dd423edbf2a3b0f556ddb4b2
Mustafa Tunc <mtunc@hotmail.com>
2018-12-22 11:33:25 -0600

    Updates

---
+++ precache-manifest.24639f4ddd7bbcbf572522831d68bad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "856377dad42ca26e44a1",
+    "url": "/static/js/main.856377da.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "856377dad42ca26e44a1",
+    "url": "/static/css/main.e3ace7d5.chunk.css"
+  },
+  {
+    "revision": "b05fc440f3fb4366c2736c1c6972563a",
+    "url": "/index.html"
+  }
+];
